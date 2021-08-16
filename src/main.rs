a72015822834395c7050120a1780ff0cc773365a
KokubunKazuya <kazup1125@me.com>
2021-08-16 23:45:21 +0900

    輪郭の抽出処理を切り出し

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use opencv::core::NormTypes::NORM_L1;
 use std::error::Error;
 
 mod pretreatment;
-
+mod contours;
 mod colors;
 
 const SOURCE_IMAGE_PATH: &str = "image.jpg";
@@ -45,47 +45,7 @@ fn main(){
 
     ////// 輪郭の抽出ここから//////
 
-    println!("輪郭抽出処理開始");
-
-    // 前処理後の画像
-    let mut src_img_pretreatment = img_pretreatment.clone();
-
-    // 抽出した輪郭の出力先を定義
-    let mut contours = VectorOfVectorOfPoint::default();
-
-    // 輪郭の抽出
-    let result_find_contours = find_contours(&src_img_pretreatment, &mut contours, RETR_LIST, CHAIN_APPROX_SIMPLE, Point::default());
-    if let Err(code) = result_find_contours {
-        println!("輪郭の抽出に失敗しました。 Message: {}", code);
-        panic!();
-    }
-
-    // 輪郭を描画した画像の出力先(元画像に輪郭を描画して出力する)
-    let mut dst_img_draw_contours;
-    let result_read_img = opencv::imgcodecs::imread(SOURCE_IMAGE_PATH, IMREAD_COLOR);
-    match result_read_img {
-        Ok(img) => dst_img_draw_contours = img,
-        Err(code) => {
-            print!("code: {:?}", code);
-            panic!();
-        }
-    };
-
-    // 輪郭の描画
-    let result_draw_contours = draw_contours(&mut dst_img_draw_contours, &contours, -1, colors::green(), 5, LINE_8, &no_array().unwrap(), INTER_MAX, Point::default());
-    if let Err(code) = result_draw_contours {
-        println!("輪郭の描画に失敗しました。 Message: {}", code);
-        panic!();
-    }
-
-    //
-    let result_write = imwrite("output_contours.jpg", &dst_img_draw_contours, &Vector::new());
-    if let Err(code) = result_write {
-        println!("輪郭描画後の出力に失敗しました。 Message: {}", code);
-        panic!();
-    }
-
-    println!("輪郭抽出処理終了");
+    let contours = contours::extract(img_pretreatment);
 
     ////// 輪郭の抽出ここまで //////
 
