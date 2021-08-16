a897cbd505956ff019ad4c65ccd55a70d113dda5
KokubunKazuya <kazup1125@me.com>
2021-08-16 23:20:10 +0900

    射影変換

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use opencv::core::{Mat, Vector, Size, Point, Scalar, BORDER_WRAP, BORDER_TRANSPARENT, BORDER_REPLICATE, CV_8UC3, no_array, VectorExtern, norm};
+use opencv::core::{Mat, Vector, Size, Point, Scalar, BORDER_WRAP, BORDER_TRANSPARENT, BORDER_REPLICATE, CV_8UC3, no_array, VectorExtern, norm, Point2f, DECOMP_LU};
 use opencv::imgcodecs::{IMREAD_GRAYSCALE, IMREAD_COLOR, imwrite};
-use opencv::imgproc::{get_structuring_element, find_contours, threshold, morphology_ex, contour_area, draw_contours, arc_length, approx_poly_dp, circle};
+use opencv::imgproc::{get_structuring_element, find_contours, threshold, morphology_ex, contour_area, draw_contours, arc_length, approx_poly_dp, circle, get_perspective_transform, warp_perspective};
 use opencv::imgproc::{THRESH_OTSU, MORPH_OPEN, MORPH_CLOSE, MORPH_RECT, RETR_CCOMP, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, INTER_MAX, LINE_8, INTER_NEAREST, RETR_LIST};
-use opencv::types::{VectorOfVectorOfPoint, VectorOfPoint};
+use opencv::types::{VectorOfVectorOfPoint, VectorOfPoint, VectorOfPoint2f};
 use opencv::core::NormTypes::NORM_L1;
 use std::error::Error;
 
@@ -208,65 +208,75 @@ fn main(){
 
     ////// 座標を左上、右上、右下、左下に分類する ここまで //////
 
-    ////// 図形が台形であるか判定 ここから //////
-    // 以下の全てが当てはまらない場合に補正を行う。
-    // 左上の頂点のX座標と左下の頂点のX座標が同じである
-    // 左上の頂点のY座標と右上の頂点のY座標が同じである
-    // 右上の頂点のX座標と右下の頂点のX座標が同じである
-    // 左下の頂点のY座標と右下の頂点のY座標が同じである
+    ////// 台形補正処理 ここから //////
 
-    let lr = is_parallel(left_up, left_down, right_up, right_down);
-    let ud = is_parallel(left_up, Point::new(347, 0), left_down, right_down);
+    let upper_line = ((right_up.x - left_up.x).abs() ^ 2) + ((right_up.y - left_up.y).abs() ^ 2);
+    let downer_line = ((right_down.x - left_down.x).abs() ^ 2) + ((right_down.y - left_down.y).abs() ^ 2);
+    let left_line = ((left_up.x - left_down.x).abs() ^ 2) + ((left_up.y - left_down.y).abs() ^ 2);
+    let right_line = ((right_up.x - right_down.x).abs() ^ 2) + ((right_up.y - right_down.y).abs() ^ 2);
 
-    // 許容誤差
-    const ALLOWABLE_ERROR: i32 = 2;
-
-    // 左上の頂点のX座標と左下の頂点のX座標が同じであるか
-    let left_up_x_eq_left_down_x = (left_up.x - ALLOWABLE_ERROR .. left_up.x + ALLOWABLE_ERROR).contains(&left_down.x);
-
-    // 左上の頂点のY座標と右上の頂点のY座標が同じであるか
-    let left_up_y_eq_right_up_y = (left_up.y - ALLOWABLE_ERROR .. left_up.y + ALLOWABLE_ERROR).contains(&right_up.y);
-
-    // 右上の頂点のX座標と右下の頂点のX座標が同じであるか
-    let right_up_x_eq_right_down_x = (right_up.x - ALLOWABLE_ERROR .. right_up.x + ALLOWABLE_ERROR).contains(&right_down.x);
-
-    // 左下の頂点のY座標と右下の頂点のY座標が同じであるか
-    let left_down_y_eq_right_down_y = (left_down.y - ALLOWABLE_ERROR .. left_down.y + ALLOWABLE_ERROR).contains(&right_down.y);
-
-    if left_up_x_eq_left_down_x || left_up_y_eq_right_up_y || right_up_x_eq_right_down_x || left_down_y_eq_right_down_y {
-        println!("台形ではありません。{:?} {:?} {:?} {:?}", left_up_x_eq_left_down_x, left_up_y_eq_right_up_y, right_up_x_eq_right_down_x, left_down_y_eq_right_down_y);
+    let max_x = if upper_line > downer_line {
+        upper_line
     } else {
-        // 台形補正を行う
-        // output_img = 台形補正
+        downer_line
+    };
+
+    let max_y = if left_line > right_line {
+        left_line
+    } else {
+        right_line
+    };
+
+    // 元となる座標をVector<Point2f>に変換する
+
+    let mut vec_vertex_points2f: Vec<Point2f> = vec![];
+    for p in vertex_points.iter() {
+        vec_vertex_points2f.push(Point2f::new(p.x as f32, p.y as f32));
+    };
+
+    let vertex_points= VectorOfPoint2f::from(vec_vertex_points2f);
+
+    let left_up = Point2f::new(0.0,0.0);
+    let left_down = Point2f::new(0.0, max_y as f32);
+    let right_down = Point2f::new(max_x as f32, max_y as f32);
+    let right_up = Point2f::new(max_x as f32, 0.0);
+
+    let coordinate: Vector<Point2f> = Vector::from(vec![left_up, left_down, right_down, right_up]);
+
+    let mut m;
+    match get_perspective_transform(&vertex_points, &coordinate, DECOMP_LU) {
+        Ok(mat) => {
+            println!("{:?}", mat);
+            m= mat;
+        },
+        Err(code) => {
+            panic!("Error. Message: {}", code)
+        }
+    };
+
+    let mut src_img;
+    let result_read_img = opencv::imgcodecs::imread(SOURCE_IMAGE_PATH, IMREAD_COLOR);
+    match result_read_img {
+        Ok(img) => src_img = img,
+        Err(code) => {
+            print!("code: {:?}", code);
+            panic!();
+        }
+    };
+
+    let mut img_corrected = Mat::default();
+    match warp_perspective(&src_img, &mut img_corrected, &m, Default::default(), 0, 0, Default::default()) {
+        Ok(_) => {
+            println!("success");
+        },
+        Err(code) => {
+            panic!("{}", code);
+        }
     }
 
-    ////// 図形が台形であるか判定 ここまで //////
+    ////// 台形補正処理 ここまで //////
 
     // 全ての処理が終わったあと、画像を出力する
     println!("画像を出力します。");
-    imwrite("output.jpg", &output_img, &Vector::new());
-}
-
-// 線分abとcdが平行かどうか
-// 参考: http://www.atelier-blue.com/contest/pc-concours/2003-h/2003h01-25.htm
-fn is_parallel(mut a: Point, mut b: Point, mut c: Point, mut d: Point) -> bool {
-    // 点bから点aを、点dから点cを引く。そうすることにより、原点からからb,dそれぞれを通る直線に平行移動できる。
-    b.x -= a.x;
-    b.y -= a.y;
-    d.x -= c.x;
-    d.y -= c.y;
-
-    return if b.x == 0 || d.x == 0 {
-        if b.x == 0 && d.x == 0 {
-            true
-        } else {
-            false
-        }
-    } else {
-        if b.y / b.x == d.y / d.x {
-            true
-        } else {
-            false
-        }
-    }
+    imwrite("output.jpg", &img_corrected, &Vector::new());
 }
